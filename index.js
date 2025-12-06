1220852b86c51909c8546ebab6207e74a83e6d08
Your Name <h.imtius10@gmail.com>
2025-12-07 02:52:51 +0600

    api for finding reuest food

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uri = "mongodb+srv://plateShareAdmin:3XgKqB0k1J61YdkI@cluster0.p9dmq2f.mongodb.net/?appName=Cluster0";
+const uri =
+    "mongodb+srv://plateShareAdmin:3XgKqB0k1J61YdkI@cluster0.p9dmq2f.mongodb.net/?appName=Cluster0";
+
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
 });
 
 app.use(cors());
@@ -19,81 +18,81 @@ app.use(express.json());
 async function run() {
     try {
         await client.connect();
-        const db = client.db('plateShare');
-        const foodCollection = db.collection('foods');
-        const userCollection = db.collection('user');
-        const requestsCollection = db.collection('foodRequests'); // <--- FIXED
+        const db = client.db("plateShare");
+        const foodCollection = db.collection("foods");
+        const requestCollection = db.collection("foodRequests");
 
-        // ------------------ FOOD ------------------
-        app.get('/foods', async (req, res) => {
-            const result = await foodCollection.find().toArray();
-            res.send(result);
+        // ------------------ FOODS ------------------
+        app.get("/foods", async (req, res) => {
+            const email = req.query.donator_email;
+            const foods = email
+                ? await foodCollection.find({ donator_email: email }).toArray()
+                : await foodCollection.find().toArray();
+            res.send(foods);
         });
 
-        app.get('/foods/:id', async (req, res) => {
+        app.get("/foods/:id", async (req, res) => {
             const id = req.params.id;
-            const result = await foodCollection.findOne({ _id: new ObjectId(id) });
+            const food = await foodCollection.findOne({ _id: new ObjectId(id) });
+            res.send(food);
+        });
+
+        app.post("/foods", async (req, res) => {
+            const food = req.body;
+            const result = await foodCollection.insertOne(food);
             res.send(result);
         });
 
-        app.post('/foods', async (req, res) => {
-            const newFood = req.body;
-            const result = await foodCollection.insertOne(newFood);
-            res.send(result);
-        });
-
-        app.delete('/foods/:id', async (req, res) => {
-            const id = req.params.id;
-            const result = await foodCollection.deleteOne({ _id: new ObjectId(id) });
-            res.send(result);
-        });
-
-        app.patch('/foods/:id', async (req, res) => {
+        app.patch("/foods/:id", async (req, res) => {
             const id = req.params.id;
             const update = req.body;
-            const result = await foodCollection.updateOne(
-                { _id: new ObjectId(id) },
-                { $set: update }
-            );
+            const result = await foodCollection.updateOne({ _id: new ObjectId(id) }, { $set: update });
+            res.send(result);
+        });
+
+        app.delete("/foods/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await foodCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
         });
 
         // ------------------ FOOD REQUESTS ------------------
         app.post("/food-requests", async (req, res) => {
             const request = req.body;
-            const result = await requestsCollection.insertOne(request);
-            res.send(result);
+            request.foodId = request.foodId.toString();
+            request.createdAt = new Date();
+            const result = await requestCollection.insertOne(request);
+            res.send({ insertedId: result.insertedId }); 
         });
 
-        app.get("/requests", async (req, res) => {
-            const foodId = req.query.foodId;
-            const requests = await requestsCollection
-                .find({ foodId })
-                .toArray();
+
+        app.get("/my-requests", async (req, res) => {
+            const email = req.query.userEmail;
+            if (!email) return res.send([]);
+            const requests = await requestCollection.find({ requester_email: email }).toArray();
             res.send(requests);
         });
 
-        app.patch("/food-requests/:id", async (req, res) => {
+        app.get("/requests/food/:foodId", async (req, res) => {
+            const foodId = req.params.foodId;
+            const requests = await requestCollection.find({ foodId }).toArray();
+            res.send(requests);
+        });
+
+        app.patch("/requests/:id", async (req, res) => {
             const id = req.params.id;
-            const update = req.body;
-            const result = await requestsCollection.updateOne(
-                { _id: new ObjectId(id) },
-                { $set: update }
-            );
+            const { status } = req.body;
+            const result = await requestCollection.updateOne({ _id: new ObjectId(id) }, { $set: { status } });
             res.send(result);
         });
 
-        console.log("MongoDB connected, server running...");
+        console.log("MongoDB Connected + API Running");
     } finally {
-        // Do not close the client
+        // Keep connection alive
     }
 }
+
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+app.get("/", (req, res) => res.send("Server is running"));
+app.listen(port, () => console.log(`Server running on port ${port}`));

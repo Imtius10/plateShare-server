a2a3a56b637da3dcf6c566061cb27e5fe696ec02
Your Name <h.imtius10@gmail.com>
2025-12-06 13:49:07 +0600

    Request Food api

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
-//3XgKqB0k1J61YdkI
-//plateShareAdmin
 
 const uri = "mongodb+srv://plateShareAdmin:3XgKqB0k1J61YdkI@cluster0.p9dmq2f.mongodb.net/?appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -16,59 +13,87 @@ const client = new MongoClient(uri, {
     }
 });
 
+app.use(cors());
+app.use(express.json());
+
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
         const db = client.db('plateShare');
         const foodCollection = db.collection('foods');
         const userCollection = db.collection('user');
+        const requestsCollection = db.collection('foodRequests'); // <--- FIXED
 
+        // ------------------ FOOD ------------------
         app.get('/foods', async (req, res) => {
-            const cursor = foodCollection.find();
-            const result = await cursor.toArray();
+            const result = await foodCollection.find().toArray();
             res.send(result);
-        })
-
-        app.post('/foods', async (req, res) => {
-            const newProdcut = req.body;
-            const result = await foodCollection.insertOne(newProdcut);
-            res.send(result);
-        })
+        });
 
         app.get('/foods/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await foodCollection.findOne(query)
-            res.send(result)
-        })
+            const result = await foodCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
 
+        app.post('/foods', async (req, res) => {
+            const newFood = req.body;
+            const result = await foodCollection.insertOne(newFood);
+            res.send(result);
+        });
 
         app.delete('/foods/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await foodCollection.deleteOne(query)
+            const result = await foodCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
-        })
+        });
 
+        app.patch('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const update = req.body;
+            const result = await foodCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: update }
+            );
+            res.send(result);
+        });
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // ------------------ FOOD REQUESTS ------------------
+        app.post("/food-requests", async (req, res) => {
+            const request = req.body;
+            const result = await requestsCollection.insertOne(request);
+            res.send(result);
+        });
+
+        app.get("/requests", async (req, res) => {
+            const foodId = req.query.foodId;
+            const requests = await requestsCollection
+                .find({ foodId })
+                .toArray();
+            res.send(requests);
+        });
+
+        app.patch("/food-requests/:id", async (req, res) => {
+            const id = req.params.id;
+            const update = req.body;
+            const result = await requestsCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: update }
+            );
+            res.send(result);
+        });
+
+        console.log("MongoDB connected, server running...");
     } finally {
-        // Ensures that the client will close when you finish/error
-       // await client.close();
+        // Do not close the client
     }
 }
 run().catch(console.dir);
 
-app.use(cors())
-app.use(express.json())
 app.get('/', (req, res) => {
-    res.send('server is running')
-})
+    res.send('Server is running');
+});
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+    console.log(`Server running on port ${port}`);
+});
